6f31f32613289363cc8e0ffde9bc0a32a8bdd60a
boconaillMIT <oconaill@mit.edu>
2025-08-20 19:36:06 -0400

    Update saml-login.js

--- netlify/functions/saml-login.js
+++ netlify/functions/saml-login.js
@@ -1,19 +1,1 @@
-// netlify/functions/saml-login.js
-exports.handler = async () => {
 
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/html' },
-    body: `
-      <html>
-        <body style="font-family: Arial, sans-serif; padding: 20px;">
-          <h1>Login Page</h1>
-          <p>Your Kerberos is <b>${kerberos}</b></p>
-          <p>Your name is <b>${name}</b></p>
-          <p>Your username is <b>${username}</b></p>
-          <p>This is a placeholder login page before real SAML integration.</p>
-        </body>
-      </html>
-    `,
-  };
-};
