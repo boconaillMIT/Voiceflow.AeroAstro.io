3b2d900dfce1d4017b6a94dfe2f1d838fa4660cf
boconaillMIT <oconaill@mit.edu>
2026-03-03 11:30:58 -0500

    Update find-similar-question.js

--- netlify/functions/find-similar-question.js
+++ netlify/functions/find-similar-question.js
@@ -56,15 +56,17 @@ exports.handler = async function(event) {
     }
 
     // Decode base64 answer
-const answer = bestRecord[QB_ANS_ID]?.value || '';
-
-return respond(200, {
-  success: true,
-  record_id: bestRecord[QB_FIELD_ID]?.value,
-  score: Math.round(bestScore * 10000) / 10000,
-  matched_question: bestRecord[QB_QUEST_ID]?.value || null,
-  answer: answer
-});
+    const answer = bestRecord[QB_ANS_ID]?.value || '';
+    const answerDecoded = Buffer.from(answer, 'base64').toString('utf-8');
+    
+    return respond(200, {
+      success: true,
+      record_id: bestRecord[QB_FIELD_ID]?.value,
+      score: Math.round(bestScore * 10000) / 10000,
+      matched_question: bestRecord[QB_QUEST_ID]?.value || null,
+      answer: answer,
+      answer_decoded: answerDecoded
+    });
 
   } catch (err) {
     return respond(500, { success: false, error: err.message });
